5be7a96a0d580c5b23392d110f6a716368d612fa
dkmsfire <leozxc2009@gmail.com>
2019-05-15 19:10:45 +0800

    add heatmap, in the future, we will use some technical to delete some variables like LDA, PCA, ...

--- Summarize.R
+++ Summarize.R
@@ -6,10 +6,22 @@ for(i in 1:5){
   assign(paste0("year", i), read.arff(paste0("data/", i, "year.arff")))
 }
 
-summary(year1)
-
 ## plot each variable to column 1
 par(mfrow = c(2,2))
 for(i in 1:64){
   plot(y = year1[[i]], x = year1[[1]], col = year1$class)
 }
+
+## heat map
+for(i in 1:64){
+  year1[[i]] = as.numeric(as.character(year1[[i]]))
+}
+year1 = as.matrix(year1)
+summary(year1)
+data = year1[,-65]
+data = as.matrix(data)
+heatmap(data)
+
+select = data[,c(7,48,49,20,37,40,46,47,50,4,17,29,57,8,10,53)]
+heatmap(select)
+## LDA
