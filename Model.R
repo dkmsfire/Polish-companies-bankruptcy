0eece70d8089099c3246f1d90251ac8aa49bfd76
dkmsfire <leozxc2009@gmail.com>
2019-05-19 14:31:36 +0800

    unbalanced data thought

---
+++ Model.R
@@ -0,0 +1,7 @@
+### In this unbalanced data , we should notice that 
+### if we guess that all of banks will not become bankruptcy
+### we can get very high accuracy.
+### So , in this classfier question , 
+### we need to use sensitivity and specificity to check our model.
+
+###
