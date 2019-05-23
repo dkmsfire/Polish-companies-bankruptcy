f5ca2da0bb82e6883992aa7dccf9b7b2ed91f4e8
dkmsfire <leozxc2009@gmail.com>
2019-05-23 10:09:30 +0800

    try to do MDS for 2d and 3d plot (3d need scatter plot)

--- Summarize.R
+++ Summarize.R
@@ -62,8 +62,16 @@ library(corrplot)
 
 for(i in 1:5){
   res = cor(bankruptcy[[i]][1:64], method = "pearson", use = "complete.obs")
-  corrplot(res, type = "upper", tl.col = "black", tl.srt = 45)
+  corrplot(res, type = "upper", tl.col = "black", tl.srt = 45, order = "hclust")
 }
 
+## MDS 2d
 
-## 
+d = dist(year1)
+fit <- cmdscale(d,eig=TRUE, k=2)
+x = fit$points[,1]
+y = fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", main="Metric MDS", type="n")
+text(x, y, labels = row.names(year1), cex = 0.7)
+
+## MDS 3d
