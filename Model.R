f9b7137295bd995c312f6f1546a2bd1c96d418b2
dkmsfire <leozxc2009@gmail.com>
2019-05-28 18:45:00 +0800

    save two kinds of data for data mining

--- Model.R
+++ Model.R
@@ -3,8 +3,25 @@
 ### we can get very high accuracy.
 ### So , in this classfier question , 
 ### we need to use sensitivity and specificity to check our model.
+library(foreign)
 
 ### knn for na imputation
+require(DMwR)
+for(i in 1:5){
+  assign(paste0("year", i), read.arff(paste0("data/", i, "year.arff")))
+}
+bankruptcy = list(year1, year2, year3, year4, year5)
+names(bankruptcy) = c("year1", "year2", "year3", "year4", "year5")
+save(bankruptcy, file = "bankruptcy.rda")
+
+for(j in 1:5){
+  bankruptcy[[j]]$year = factor(j)
+}
+bankruptcy_all = bankruptcy$year1
+for(k in 2:5){
+  bankruptcy_all = rbind(bankruptcy_all, bankruptcy[[k]])
+}
+save(bankruptcy_all, file = "bankruptcy_all.rda")
 
 ### smote sampling for unbalanced data
 
